79f2047b7895fac9036652379593406e226b545a
AxelR <axel.ronnberg.25@gmail.com>
2023-12-14 17:36:29 +0100

    start of lab 4 implementation

---
+++ lab4/lab4.sql
@@ -0,0 +1,196 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS airport;
+DROP TABLE IF EXISTS booking;
+DROP TABLE IF EXISTS contactpassenger;
+DROP TABLE IF EXISTS creditcard;
+DROP TABLE IF EXISTS flight;
+DROP TABLE IF EXISTS ispartof;
+DROP TABLE IF EXISTS passenger;
+DROP TABLE IF EXISTS reservation;
+DROP TABLE IF EXISTS route;
+DROP TABLE IF EXISTS ticket;
+DROP TABLE IF EXISTS weekday;
+DROP TABLE IF EXISTS weekschedule;
+DROP TABLE IF EXISTS year;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+create table airport
+(
+    IATA    varchar(3)  not null
+        primary key,
+    Name    varchar(30) null,
+    City    varchar(30) null,
+    Country varchar(30) null
+)
+    comment 'The porting of the air';
+
+create table creditcard
+(
+    CardNumber bigint      not null
+        primary key,
+    FirstName  varchar(30) null,
+    LastName   varchar(30) null
+);
+
+create table flight
+(
+    WeekdaySchedule int null,
+    FlightNumber    int not null
+        primary key,
+    WeekNr          int null
+);
+
+create table passenger
+(
+    PassportNumber int         not null
+        primary key,
+    Firstname      varchar(30) null,
+    Lastname       varchar(30) null
+);
+
+create table contactpassenger
+(
+    Passenger   int         not null
+        primary key,
+    Email       varchar(30) null,
+    PhoneNumber bigint      null,
+    constraint ContactPassenger_passenger_PassportNumber_fk
+        foreign key (Passenger) references passenger (PassportNumber)
+);
+
+create table reservation
+(
+    ReservationNumber int    not null
+        primary key,
+    Price             double null,
+    Flight            int    null,
+    ContactPassenger  int    null,
+    constraint reservation_contactpassenger_Passenger_fk
+        foreign key (ContactPassenger) references contactpassenger (Passenger),
+    constraint reservation_flight_FlightNumber_fk
+        foreign key (Flight) references flight (FlightNumber)
+);
+
+create table booking
+(
+    Reservation int    not null
+        primary key,
+    PricePaid   double null,
+    CreditCard  bigint null,
+    constraint booking_reservation_ReservationNumber_fk
+        foreign key (Reservation) references reservation (ReservationNumber)
+);
+
+create table ispartof
+(
+    Reservation int not null,
+    Passenger   int not null,
+    primary key (Reservation, Passenger),
+    constraint ispartof_passenger_PassportNumber_fk
+        foreign key (Passenger) references passenger (PassportNumber),
+    constraint ispartof_reservation_ReservationNumber_fk
+        foreign key (Reservation) references reservation (ReservationNumber)
+);
+
+create table ticket
+(
+    TicketNumber int not null
+        primary key,
+    Passenger    int null,
+    Booking      int null,
+    Flight       int null,
+    constraint ticket_booking_Reservation_fk
+        foreign key (Booking) references booking (Reservation),
+    constraint ticket_flight_FlightNumber_fk
+        foreign key (Flight) references flight (FlightNumber),
+    constraint ticket_passenger_PassportNumber_fk
+        foreign key (Passenger) references passenger (PassportNumber)
+);
+
+create table year
+(
+    Year         int    not null
+        primary key,
+    ProfitFactor double null
+);
+
+create table route
+(
+    RouteID            int        auto_increment
+        primary key,
+    OriginAirport      varchar(3) null,
+    DestinationAirport varchar(3) null,
+    Price              double     null,
+    year               int        null,
+    constraint Route_airport_IATA_fk
+        foreign key (OriginAirport) references airport (IATA),
+    constraint Route_airport_IATA_fk2
+        foreign key (DestinationAirport) references airport (IATA),
+    constraint route_year_Year_fk
+        foreign key (year) references year (Year)
+)
+    comment 'The highways in the sky for the planes to drive on';
+
+create table weekday
+(
+    Name      varchar(30) not null
+        primary key,
+    DayFactor double      null,
+    IsOnYear  int         null,
+    constraint weekday_year_Year_fk
+        foreign key (IsOnYear) references year (Year)
+);
+
+create table weekschedule
+(
+    WeekScheduleID int         auto_increment
+        primary key,
+    onDay          varchar(30) null,
+    DepartureTime  time        null,
+    ProfitFactor   double      null,
+    Route          int         null,
+    constraint WeekSchedule_route_RouteID_fk
+        foreign key (Route) references route (RouteID),
+    constraint WeekSchedule_weekday_Name_fk
+        foreign key (onDay) references weekday (Name)
+);
+
+
+delimiter //
+CREATE PROCEDURE addYear (in year int, in factor double)
+BEGIN
+    INSERT INTO year (Year, ProfitFactor) VALUES (year, factor);
+END;
+delimiter ;
+
+delimiter //
+CREATE PROCEDURE addDay (in year int, in day VARCHAR(30), in factor double)
+BEGIN
+    INSERT INTO weekday (Name, DayFactor, IsOnYear) VALUES (day, factor, year);
+END;
+delimiter ;
+
+delimiter //
+CREATE PROCEDURE addDestination (in code VARCHAR(3), in name VARCHAR(30), in country VARCHAR(30))
+BEGIN
+    INSERT INTO airport (IATA, Name, Country) VALUES (code, name, country);
+END;
+delimiter ;
+
+delimiter //
+CREATE PROCEDURE addRoute (in departure_airport_code VARCHAR(3), in arrival_airport_code VARCHAR(3), in year int, in routeprice double)
+BEGIN
+    INSERT INTO route (OriginAirport, DestinationAirport, Price, year) VALUES (departure_airport_code, arrival_airport_code, routeprice, year);
+END;
+delimiter ;
+
+delimiter //
+CREATE PROCEDURE addFlight (in departure_airport_code VARCHAR(3), in arrival_airport_code VARCHAR(3), in yearin int, in day VARCHAR(30), in departuretime TIME)
+BEGIN
+    INSERT INTO weekschedule (onDay, DepartureTime, Route) VALUES (day, departuretime, (SELECT route.RouteID FROM route WHERE DestinationAirport = arrival_airport_code and OriginAirport = departure_airport_code and year = yearin));
+    SET @y = LAST_INSERT_ID();
+    INSERT INTO flight (WeekdaySchedule, FlightNumber, WeekNr) VALUES (@y.WeekScheduleID);
+END;
+delimiter ;
