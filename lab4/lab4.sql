e65a3cf4765ed61938785d5aa5ddaee9def2c95b
Eskil Brännerud <eskbr129@student.liu.se>
2023-12-19 14:51:44 +0100

    done with addReservation

--- lab4/lab4.sql
+++ lab4/lab4.sql
@@ -14,14 +14,17 @@ DROP TABLE IF EXISTS weekday;
 DROP TABLE IF EXISTS weekschedule;
 DROP TABLE IF EXISTS year;
 
-DROP PROCEDURE IF EXISTS addDay
-DROP PROCEDURE IF EXISTS addYear
-DROP PROCEDURE IF EXISTS addDestination
-DROP PROCEDURE IF EXISTS addRoute
-DROP PROCEDURE IF EXISTS addFlight
+DROP PROCEDURE IF EXISTS addDay;
+DROP PROCEDURE IF EXISTS addYear;
+DROP PROCEDURE IF EXISTS addDestination;
+DROP PROCEDURE IF EXISTS addRoute;
+DROP PROCEDURE IF EXISTS addFlight;
+DROP PROCEDURE IF EXISTS addReservation;
 
-DROP FUNCTION IF EXISTS calculateFreeSeats
-DROP FUNCTION IF EXISTS calculatePrice
+DROP FUNCTION IF EXISTS calculateFreeSeats;
+DROP FUNCTION IF EXISTS calculatePrice;
+
+DROP TRIGGER IF EXISTS after_booking_insert;
 
 SET FOREIGN_KEY_CHECKS=1;
 
@@ -71,7 +74,7 @@ create table contactpassenger
 
 create table reservation
 (
-    ReservationNumber int    not null
+    ReservationNumber int    auto_increment
         primary key,
     Price             double null,
     Flight            int    null,
@@ -218,7 +221,7 @@ BEGIN
     RETURN freeseats;
 END;
 
-CREATE FUNCTION calculatePrice(flightnumber INT) 
+CREATE FUNCTION calculatePrice(flightnumber INT)
 RETURNS DOUBLE
 BEGIN
     DECLARE price DOUBLE;
@@ -233,73 +236,73 @@ BEGIN
     return price;
 END;
 
+
+
 DELIMITER //
 CREATE TRIGGER after_booking_insert
 AFTER INSERT ON booking
 FOR EACH ROW
 BEGIN
-    SET @getPassenger CURSOR FOR;
-    SELECT passenger
-    FROM ispartof
-    WHERE Reservation = NEW.ReservationNumber;
+  DECLARE passengerCount INT DEFAULT 0;
+  DECLARE currentPassenger INT DEFAULT 0;
 
-    OPEN @getPassenger
-    FETCH NEXT
-    FROM @getPassenger INTO @reservation, @passenger
-    WHILE @@FETCH_STATUS = 0
-    BEGIN 
-        INSERT INTO ticket CAST(RAND(2)*2147483647 AS INT), @passenger, @reservation, NEW.Reservation.Flight
-        FETCH NEXT
-        FROM @getPassenger INTO @reservation, @passenger
-    END
+  SELECT COUNT(*) INTO passengerCount FROM ispartof WHERE Reservation = NEW.Reservation;
+
+  WHILE passengerCount > 0 DO
+    SELECT Passenger INTO currentPassenger FROM ispartof WHERE Reservation = NEW.Reservation LIMIT 1;
+
+    INSERT INTO ticket(TicketNumber, Passenger, Booking)
+    VALUES (FLOOR(RAND() * 1000000), currentPassenger, NEW.Reservation);
+
+    SET passengerCount = passengerCount - 1;
+  END WHILE;
 END;
+//
 DELIMITER ;
 
 
+delimiter //
+CREATE PROCEDURE addReservation (in departure_airport_code VARCHAR(3), in arrival_airport_code VARCHAR(3), in year int, in week INT, in day VARCHAR(30), in departure_time TIME, in number_of_passengers INT, out reservation_number INT)
+BEGIN
+    DECLARE @flight_number INT DEFAULT NULL;
+    DECLARE @free_seats INT DEFAULT 0;
 
+    SELECT flight.FlightNumber INTO @flight_number FROM flight 
+        LEFT JOIN weekschedule ON weekschedule.WeekScheduleID = flight.WeekdaySchedule
+        LEFT JOIN route ON weekschedule.Route = route.RouteID
+        LEFT JOIN weekday ON weekschedule.onDay = weekday.Name
+        LEFT JOIN year ON weekday.IsOnYear = year.Year
+    WHERE route.OriginAirport = departure_airport_code
+    AND route.DestinationAirport = arrival_airport_code
+    AND weekschedule.onDay = day
+    AND weekday.IsOnYear = year
+    AND flight.WeekNr = week
+    AND weekschedule.DepartureTime = departure_time;
 
-/*************************************************************************************
- Question 3, Flight addition interfaces test script.
- This is a test script that tests that the interface of the BryanAir back-end works
- correctly. It simply tests that the interfaces exists for filling the database with
- flightinformation, without acutally checking that they do what they are supposed to. 
- This is done later in the question 7 testscript.
-*************************************************************************************/
-SELECT "Testing questions for flight addition procedures, i.e. question 3" as "Message";
-SELECT "Expected output for all queries are 'Query OK, 1 row affected (0.00 sec)' (where the time might differ)" as "Message";
+    IF @flight_number IS NULL THEN
+        SELECT "There exist no flight for the given route, date and time" AS "Message";
+        SET reservation_number = NULL;
+    END IF;
 
-/*Fill the database with data */
-SELECT "Trying to add 2 years" AS "Message";
-CALL addYear(2010, 2.3);
-CALL addYear(2011, 2.5);
-SELECT "Trying to add 4 days" AS "Message";
-CALL addDay(2010,"Monday",1);
-CALL addDay(2010,"Tuesday",1.5);
-CALL addDay(2011,"Saturday",2);
-CALL addDay(2011,"Sunday",2.5);
-SELECT "Trying to add 2 destinations" AS "Message";
-CALL addDestination("MIT","Minas Tirith","Mordor");
-CALL addDestination("HOB","Hobbiton","The Shire");
-SELECT "Trying to add 4 routes" AS "Message";
-CALL addRoute("MIT","HOB",2010,2000);
-CALL addRoute("HOB","MIT",2010,1600);
-CALL addRoute("MIT","HOB",2011,2100);
-CALL addRoute("HOB","MIT",2011,1500);
-SELECT "Trying to add 4 weeklyschedule flights" AS "Message";
-CALL addFlight("MIT","HOB", 2010, "Monday", "09:00:00");
-CALL addFlight("HOB","MIT", 2010, "Tuesday", "10:00:00");
-CALL addFlight("MIT","HOB", 2011, "Sunday", "11:00:00");
-CALL addFlight("HOB","MIT", 2011, "Sunday", "12:00:00");
+    SELECT calculateFreeSeats(@flight_number) INTO @free_seats;
 
-SELECT "You are now supposed to have 208 flights in your database. If so, and with reasonable data, it is probably correct and this is further tested for question 7" as "Message";
+    IF @free_seats < number_of_passengers THEN
+        SELECT "There are not enough seats available on the chosen flight" AS "Message";
+        SET reservation_number = NULL;
+    ELSE
+        INSERT INTO reservation (Price, Flight) VALUES (calculatePrice(@flight_number), @flight_number);
+        SET reservation_number = LAST_INSERT_ID();
+    END IF;
+END;
+delimiter ;
 
 
 
 /******************************************************************************************
  Question 6, Reservation interfaces test script.
  This is a test script that tests that the interface of the BryanAir back-end works
- correctly. More specifically it tests that the interfaces handling the reservation 
- procedures works correctly and give the correct output. 
+ correctly. More specifically it tests that the interfaces handling the reservation
+ procedures works correctly and give the correct output.
  The test-cases are:
 	Test1:  Adding a correct reservation.
 	Test2:  Adding a reservation with incorrect flight details. Should print the
@@ -310,16 +313,16 @@ SELECT "You are now supposed to have 208 flights in your database. If so, and wi
 	Test5:  Adding a passenger with incorrect reservation number. Should print the
  		message: "The given reservation number does not exist".
 	Test6:  Adding a contact in a correct manner.
-	Test7:  Adding a contact with incorrect reservation number. Should print the 
+	Test7:  Adding a contact with incorrect reservation number. Should print the
 		message "The given reservation number does not exist".
 	Test8:  Adding a contact that is not a passenger on the reservation. Should print
 		the message "The person is not a passenger of the reservation".
-	Test9:  Making a payment in a correct manner. 
-	Test10: Making a payment to a reservation with incorrect reservation number. 
-		Should print the message "The given reservation number does not exist". 
-	Test11: Making a payment to a reservation with no contact. Should print the message 
+	Test9:  Making a payment in a correct manner.
+	Test10: Making a payment to a reservation with incorrect reservation number.
+		Should print the message "The given reservation number does not exist".
+	Test11: Making a payment to a reservation with no contact. Should print the message
 		"The reservation has no contact yet".
-	Test12: Adding a passenger to an already payed reservation. Should print the message 
+	Test12: Adding a passenger to an already payed reservation. Should print the message
 		"The booking has already been payed and no futher passengers can be added".
 **********************************************************************************************/
 SELECT "Testing answer for 6, handling reservations and bookings" as "Message";
@@ -335,13 +338,13 @@ CALL addFlight("MIT","HOB", 2010, "Monday", "21:00:00");
 
 SELECT "Test 1: Adding a reservation, expected OK result" as "Message";
 CALL addReservation("MIT","HOB",2010,1,"Monday","09:00:00",3,@a);
-SELECT "Check that the reservation number is returned properly (any number will do):" AS "Message",@a AS "Res. number returned"; 
+SELECT "Check that the reservation number is returned properly (any number will do):" AS "Message",@a AS "Res. number returned";
 
 SELECT "Test 2: Adding a reservation with incorrect flightdetails. Expected answer: There exist no flight for the given route, date and time" as "Message";
-CALL addReservation("MIT","HOB",2010,1,"Tuesday","21:00:00",3,@b); 
+CALL addReservation("MIT","HOB",2010,1,"Tuesday","21:00:00",3,@b);
 
 SELECT "Test 3: Adding a reservation when there are not enough seats. Expected answer: There are not enough seats available on the chosen flight" as "Message";
-CALL addReservation("MIT","HOB",2010,1,"Monday","09:00:00",61,@c); 
+CALL addReservation("MIT","HOB",2010,1,"Monday","09:00:00",61,@c);
 
 SELECT "Test 4.1: Adding a passenger. Expected OK result" as "Message";
 CALL addPassenger(@a,00000001,"Frodo Baggins");
@@ -351,35 +354,35 @@ CALL addReservation("MIT","HOB",2010,1,"Monday","21:00:00",4,@e);
 SELECT @e AS "Reservation number";
 
 SELECT "Now testing. Expected OK result" as "Message";
-CALL addPassenger(@e,00000001,"Frodo Baggins"); 
+CALL addPassenger(@e,00000001,"Frodo Baggins");
 
 SELECT "Test 5: Adding a passenger with incorrect reservation number. Expected result: The given reservation number does not exist" as "Message";
-CALL addPassenger(9999999,00000001,"Frodo Baggins"); 
+CALL addPassenger(9999999,00000001,"Frodo Baggins");
 
 SELECT "Test 6: Adding a contact. Expected OK result" as "Message";
-CALL addContact(@a,00000001,"frodo@magic.mail",080667989); 
+CALL addContact(@a,00000001,"frodo@magic.mail",080667989);
 
 SELECT "Test 7: Adding a contact with incorrect reservation number. Expected result: The given reservation number does not exist" as "Message";
-CALL addContact(99999,00000001,"frodo@magic.mail",080667989); 
+CALL addContact(99999,00000001,"frodo@magic.mail",080667989);
 
 SELECT "Test 8: Adding a contact that is not a passenger on the reservation. Expected result: The person is not a passenger of the reservation" as "Message";
-CALL addContact(@a,00000099,"frodo@magic.mail",080667989); 
+CALL addContact(@a,00000099,"frodo@magic.mail",080667989);
 
 SELECT "Test 9: Making a payment. Expected OK result" as "Message";
-CALL addPayment (@a, "Gandalf", 6767676767676767); 
+CALL addPayment (@a, "Gandalf", 6767676767676767);
 
 SELECT "Test 10: Making a payment to a reservation with incorrect reservation number. Expected result: The given reservation number does not exist" as "Message";
 CALL addPayment (999999, "Gandalf", 6767676767676767);
 
 SELECT "Test 11: Making a payment to a reservation with no contact. First setting up reservation" as "Message";
-CALL addReservation("MIT","HOB",2010,1,"Monday","09:00:00",1,@d); 
-CALL addPassenger(@d,00000002,"Sam Gamgee"); 
+CALL addReservation("MIT","HOB",2010,1,"Monday","09:00:00",1,@d);
+CALL addPassenger(@d,00000002,"Sam Gamgee");
 
 SELECT "Now testing. Expected result: The reservation has no contact yet" as "Message";
-CALL addPayment (@d, "Gandalf", 6767676767676767); 
+CALL addPayment (@d, "Gandalf", 6767676767676767);
 
 SELECT "Test 12: Adding a passenger to an already payed reservation. Expected result: The booking has already been payed and no futher passengers can be added" as "Message";
-CALL addPassenger(@a,00000003,"Merry Pippins"); 
+CALL addPassenger(@a,00000003,"Merry Pippins");
 
 
 /******************************************************************************************
@@ -391,48 +394,48 @@ CALL addPassenger(@a,00000003,"Merry Pippins");
 SELECT "Test 13: Testing if an overbooking occurs" as "Message";
 SELECT "Preparing the reservation:" as "Message";
 /*Fill the database with data */
-CALL addReservation("MIT","HOB",2010,1,"Monday","21:00:00",3,@a); 
-CALL addPassenger(@a,13000001,"Saruman"); 
-CALL addPassenger(@a,13000002,"Orch1"); 
-CALL addPassenger(@a,13000003,"Orch2"); 
-CALL addPassenger(@a,13000004,"Orch3"); 
-CALL addPassenger(@a,13000005,"Orch4"); 
-CALL addPassenger(@a,13000006,"Orch5"); 
-CALL addPassenger(@a,13000007,"Orch6"); 
-CALL addPassenger(@a,13000008,"Orch7"); 
-CALL addPassenger(@a,13000009,"Orch8"); 
-CALL addPassenger(@a,13000010,"Orch9"); 
-CALL addPassenger(@a,13000011,"Orch10"); 
-CALL addPassenger(@a,13000012,"Orch11"); 
-CALL addPassenger(@a,13000013,"Orch12"); 
-CALL addPassenger(@a,13000014,"Orch13"); 
-CALL addPassenger(@a,13000015,"Orch14"); 
-CALL addPassenger(@a,13000016,"Orch15"); 
-CALL addPassenger(@a,13000017,"Orch16"); 
-CALL addPassenger(@a,13000018,"Orch17"); 
-CALL addPassenger(@a,13000019,"Orch18"); 
-CALL addPassenger(@a,13000020,"Orch19"); 
+CALL addReservation("MIT","HOB",2010,1,"Monday","21:00:00",3,@a);
+CALL addPassenger(@a,13000001,"Saruman");
+CALL addPassenger(@a,13000002,"Orch1");
+CALL addPassenger(@a,13000003,"Orch2");
+CALL addPassenger(@a,13000004,"Orch3");
+CALL addPassenger(@a,13000005,"Orch4");
+CALL addPassenger(@a,13000006,"Orch5");
+CALL addPassenger(@a,13000007,"Orch6");
+CALL addPassenger(@a,13000008,"Orch7");
+CALL addPassenger(@a,13000009,"Orch8");
+CALL addPassenger(@a,13000010,"Orch9");
+CALL addPassenger(@a,13000011,"Orch10");
+CALL addPassenger(@a,13000012,"Orch11");
+CALL addPassenger(@a,13000013,"Orch12");
+CALL addPassenger(@a,13000014,"Orch13");
+CALL addPassenger(@a,13000015,"Orch14");
+CALL addPassenger(@a,13000016,"Orch15");
+CALL addPassenger(@a,13000017,"Orch16");
+CALL addPassenger(@a,13000018,"Orch17");
+CALL addPassenger(@a,13000019,"Orch18");
+CALL addPassenger(@a,13000020,"Orch19");
 CALL addPassenger(@a,13000021,"Orch20");
-CALL addPassenger(@a,13000022,"Orch21"); 
-CALL addPassenger(@a,13000023,"Orch22"); 
-CALL addPassenger(@a,13000024,"Orch23"); 
-CALL addPassenger(@a,13000025,"Orch24"); 
-CALL addPassenger(@a,13000026,"Orch25"); 
-CALL addPassenger(@a,13000027,"Orch26"); 
-CALL addPassenger(@a,13000028,"Orch27"); 
-CALL addPassenger(@a,13000029,"Orch28"); 
-CALL addPassenger(@a,13000030,"Orch29"); 
+CALL addPassenger(@a,13000022,"Orch21");
+CALL addPassenger(@a,13000023,"Orch22");
+CALL addPassenger(@a,13000024,"Orch23");
+CALL addPassenger(@a,13000025,"Orch24");
+CALL addPassenger(@a,13000026,"Orch25");
+CALL addPassenger(@a,13000027,"Orch26");
+CALL addPassenger(@a,13000028,"Orch27");
+CALL addPassenger(@a,13000029,"Orch28");
+CALL addPassenger(@a,13000030,"Orch29");
 CALL addPassenger(@a,13000031,"Orch30");
-CALL addPassenger(@a,13000032,"Orch31"); 
-CALL addPassenger(@a,13000033,"Orch32"); 
-CALL addPassenger(@a,13000034,"Orch33"); 
-CALL addPassenger(@a,13000035,"Orch34"); 
-CALL addPassenger(@a,13000036,"Orch35"); 
-CALL addPassenger(@a,13000037,"Orch36"); 
-CALL addPassenger(@a,13000038,"Orch37"); 
-CALL addPassenger(@a,13000039,"Orch38"); 
-CALL addPassenger(@a,13000040,"Orch39"); 
+CALL addPassenger(@a,13000032,"Orch31");
+CALL addPassenger(@a,13000033,"Orch32");
+CALL addPassenger(@a,13000034,"Orch33");
+CALL addPassenger(@a,13000035,"Orch34");
+CALL addPassenger(@a,13000036,"Orch35");
+CALL addPassenger(@a,13000037,"Orch36");
+CALL addPassenger(@a,13000038,"Orch37");
+CALL addPassenger(@a,13000039,"Orch38");
+CALL addPassenger(@a,13000040,"Orch39");
 CALL addPassenger(@a,13000041,"Orch40");
-CALL addContact(@a,13000001,"saruman@magic.mail",080667989); 
+CALL addContact(@a,13000001,"saruman@magic.mail",080667989);
 SELECT "Now testing. Expected result: There are not enough seats available on the flight anymore, deleting reservation" as "Message";
-CALL addPayment (@a, "Sauron",7878787878); 
+CALL addPayment (@a, "Sauron",7878787878);
