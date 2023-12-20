208e89109d049ca1f443a09500428ed7dc602f3f
Eskil Brännerud <eskbr129@student.liu.se>
2023-12-20 18:58:57 +0100

    done with lab4

--- lab4/lab4.sql
+++ lab4/lab4.sql
@@ -226,9 +226,13 @@ CREATE FUNCTION calculateFreeSeats(flightnumber INT)
 RETURNS INT
 BEGIN
     DECLARE freeseats INT DEFAULT 0;
+    SELECT 40 - COUNT(*) INTO freeseats FROM ticket
+    WHERE Flight = flightnumber;
+    /*
     SELECT 40 - COUNT(*) INTO freeseats FROM ispartof
         LEFT JOIN reservation ON reservation.ReservationNumber = ispartof.Reservation
     WHERE reservation.Flight = flightnumber;
+    */
     RETURN freeseats;
 END;
 //
@@ -260,19 +264,27 @@ CREATE TRIGGER after_booking_insert
 AFTER INSERT ON booking
 FOR EACH ROW
 BEGIN
-  DECLARE passengerCount INT DEFAULT 0;
-  DECLARE currentPassenger INT DEFAULT 0;
+    DECLARE passengerCount INT DEFAULT 0;
+    DECLARE currentPassenger INT DEFAULT 0;
 
-  SELECT COUNT(*) INTO passengerCount FROM ispartof WHERE Reservation = NEW.Reservation;
+    SELECT COUNT(*) INTO passengerCount FROM ispartof WHERE Reservation = NEW.Reservation;
 
-  WHILE passengerCount > 0 DO
-    SELECT Passenger INTO currentPassenger FROM ispartof WHERE Reservation = NEW.Reservation LIMIT 1;
+    INSERT INTO ticket (TicketNumber, Passenger, Booking, Flight) 
+    SELECT FLOOR(RAND() * 1000000), Passenger, Reservation, reservation.flight 
+    FROM ispartof 
+        LEFT JOIN reservation ON reservation.ReservationNumber = ispartof.Reservation
+    WHERE ispartof.Reservation = NEW.Reservation;
 
-    INSERT INTO ticket(TicketNumber, Passenger, Booking)
-    VALUES (FLOOR(RAND() * 1000000), currentPassenger, NEW.Reservation);
+    /*
+    WHILE passengerCount > 0 DO
+        SELECT Passenger INTO currentPassenger FROM ispartof WHERE Reservation = NEW.Reservation AND Passenger != currentPassenger LIMIT 1;
 
-    SET passengerCount = passengerCount - 1;
-  END WHILE;
+        INSERT INTO ticket(TicketNumber, Passenger, Booking)
+        VALUES (FLOOR(RAND() * 1000000), currentPassenger, NEW.Reservation);
+
+        SET passengerCount = passengerCount - 1;
+    END WHILE;
+    */
 END;
 //
 DELIMITER ;
@@ -369,11 +381,14 @@ BEGIN
         DECLARE c INT DEFAULT 0;
         DECLARE r INT DEFAULT 0;
         DECLARE cc INT DEFAULT NULL;
+        DECLARE p INT DEFAULT 0;
+        DECLARE pp INT DEFAULT 0;
 
         SELECT COUNT(*) INTO c FROM creditcard WHERE CardNumber = credit_card_number;
         IF c = 0 THEN
             INSERT INTO creditcard (CardNumber, FirstName) VALUES (credit_card_number, cardholder_name);
         END IF;
+
 
         SELECT COUNT(*), ContactPassenger INTO r, cc FROM reservation WHERE ReservationNumber = Reservation_Number;
 
@@ -383,7 +398,17 @@ BEGIN
             IF cc IS NULL THEN
                 SELECT "The given reservation number does not have a contact" AS "Message";
             ELSE
-                UPDATE booking SET PricePaid = (SELECT Price FROM reservation WHERE ReservationNumber = Reservation_Number), CreditCard = credit_card_number WHERE Reservation = Reservation_Number;
+                SELECT COUNT(*), calculateFreeSeats(flight.FlightNumber) 
+                INTO p, pp 
+                FROM ispartof 
+                    LEFT JOIN reservation ON ispartof.Reservation = reservation.ReservationNumber
+                    LEFT JOIN flight ON reservation.Flight = flight.FlightNumber
+                WHERE Reservation = Reservation_Number;
+                IF p <= pp THEN
+                    INSERT INTO booking (Reservation, PricePaid, CreditCard) VALUES (Reservation_Number, (SELECT Price FROM reservation WHERE ReservationNumber = Reservation_Number), credit_card_number);
+                ELSE
+                    SELECT "The given reservation number does not have a contact" AS "Message";
+                END IF;
             END IF;
         END IF;
 END;
@@ -410,72 +435,3 @@ FROM flight
 
 //
 delimiter ;
-
-/*
-delimiter //
-CREATE VIEW allFlights AS
-SELECT f.WeekNr AS "departure_week",
-w.DepartureTime AS "departure_time",
-w.onDay AS "departure_day",
-ww.IsOnYear AS "departure_year",
-a.Name AS "departure_city_name",
-b.Name AS "destination_city_name",
-calculateFreeSeats(f.FlightNumber) AS "nr_of_free_seats",
-calculatePrice(f.FlightNumber) AS "current_price_per_seat"
-FROM weekschedule w, airport a, airport b, route r, flight f, weekday ww WHERE a.IATA = r.OriginAirport AND b.IATA = r.DestinationAirport AND f.WeekdaySchedule = w.WeekScheduleID AND w.Route = r.RouteID AND w.onDay = ww.Name;
-//
-delimiter ;
-*/
-
-
-/******************************************************************************************
- Question 7, Correct representation in the view.
- This is a test script that tests that the interface of the BryanAir back-end works
- correctly. More specifically it tests that flights and reservations are added correctly and
- that the number of seats and price is calculated correctly. This is done by checking against a
- previous (correct) response to the query from an external database.
-**********************************************************************************************/
-
-SELECT "Checking that bookings and flights are added correctly by checking the view" as "Message";
-/*Fill the database with flights */
-SELECT "Step1, fill the database with flights" AS "Message";
-CALL addYear(2010, 2.3);
-CALL addYear(2011, 2.5);
-CALL addDay(2010,"Monday",1);
-CALL addDay(2010,"Tuesday",1.5);
-CALL addDay(2011,"Saturday",2);
-CALL addDay(2011,"Sunday",2.5);
-CALL addDestination("MIT","Minas Tirith","Mordor");
-CALL addDestination("HOB","Hobbiton","The Shire");
-CALL addRoute("MIT","HOB",2010,2000);
-CALL addRoute("HOB","MIT",2010,1600);
-CALL addRoute("MIT","HOB",2011,2100);
-CALL addRoute("HOB","MIT",2011,1500);
-CALL addFlight("MIT","HOB", 2010, "Monday", "09:00:00");
-CALL addFlight("HOB","MIT", 2010, "Tuesday", "10:00:00");
-CALL addFlight("MIT","HOB", 2011, "Sunday", "11:00:00");
-CALL addFlight("HOB","MIT", 2011, "Sunday", "12:00:00");
-
-SELECT "Step2, add a bunch of bookings to the flights" AS "Message";
-CALL addReservation("MIT","HOB",2010,1,"Monday","09:00:00",3,@a);
-CALL addPassenger(@a,00000001,"Frodo Baggins");
-CALL addContact(@a,00000001,"frodo@magic.mail",080667989);
-CALL addPassenger(@a,00000002,"Sam Gamgee");
-CALL addPassenger(@a,00000003,"Merry Pippins");
-CALL addPayment (@a, "Gandalf", 6767);
-CALL addReservation("MIT","HOB",2010,1,"Monday","09:00:00",3,@b);
-CALL addPassenger(@b,00000011,"Nazgul1");
-CALL addContact(@b,00000011,"Nazgul@darkness.mail",666);
-CALL addPassenger(@b,00000012,"Nazgul2");
-CALL addPassenger(@b,00000013,"Nazgul3");
-CALL addPayment (@b, "Saruman", 6868);
-
-SELECT "Step3, check that the results are correct. If so the next query should return the empty set. If any line is returned then this is either missing, incorrect or additional to what the database should contain" AS "Message";
-SELECT departure_city_name, destination_city_name, departure_time, departure_day,departure_week, departure_year, nr_of_free_seats, current_price_per_seat
-FROM (
-SELECT departure_city_name, destination_city_name, departure_time, departure_day,departure_week, departure_year, nr_of_free_seats, current_price_per_seat FROM allFlights
-UNION ALL
-SELECT departure_city_name, destination_city_name, departure_time, departure_day,departure_week, departure_year, nr_of_free_seats, current_price_per_seat FROM TDDD37.Question7CorrectResult
-) res
-GROUP BY departure_city_name, destination_city_name, departure_time, departure_day,departure_week, departure_year, nr_of_free_seats, current_price_per_seat
-HAVING count(*) = 1;
